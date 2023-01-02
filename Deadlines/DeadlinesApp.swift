24cea4a948d636b382514b32f254b767f9430125
buscaroli <m.buscaroli@outlook.com>
2023-01-02 12:40:58 +0000

    Initial Commit

---
+++ Deadlines/DeadlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeadlinesApp.swift
+//  Deadlines
+//
+//  Created by Matteo on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeadlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
